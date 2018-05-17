f00b26b99f7cf98be993092f0c749a87af3d8ee6
Rachel Healey <rhwork@Rachels-MacBook-Pro.local>
2018-05-17 13:04:34 +1000

    refactor boilerplate

--- README.md
+++
@@ -1,3 +0,0 @@
-# RK-React
-
-#### Simple, clean, fully tested react components
--- demo/src/button.js
+++
@@ -1,8 +0,0 @@
-import { action } from '@storybook/addon-actions';
-// eslint-disable-next-line  import/no-extraneous-dependencies
-import { Button } from 'rk-react';
-import { storiesOf } from '@storybook/react';
-import React from 'react';
-
-storiesOf('Button', module)
-  .add('with black theme', () => <Button onClick={action('clicked')} theme="black">Hello Button</Button>);
