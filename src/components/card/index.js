ed8c60d98d334265cdda0fb308c609f73d156ee0
Rachel Healey <rhwork@Rachels-MacBook-Pro.local>
2018-05-17 17:36:38 +1000

    add styled components

--- src/components/card/index.js
+++ src/components/card/index.js
@@ -1,14 +1,10 @@
-import PropTypes from 'prop-types';
-import React from 'react';
+import styled from 'styled-components';
 
-const Card = ({ children }) => (
-  <div>
-    {children}
-  </div>
-);
-
-Card.propTypes = {
-  children: PropTypes.node.isRequired,
-};
+const Card = styled.div`
+  background-color: #fff;
+  box-shadow: 0 1px 3px rgba(0, 0, 0, 0.12), 0 1px 2px rgba(0, 0, 0, 0.24);
+  margin: 1rem 0;
+  padding: 1rem;
+`;
 
 export default Card;
