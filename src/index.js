28740fa16ea64b97c52f91a2393b785bff01a0e1
rkhealey <hello@rkhealey.com>
2018-07-25 13:06:08 +1000

    update icon

--- src/index.js
+++ src/index.js
@@ -1,11 +1,13 @@
 import { Column, Row, Wrapper } from './components/grid';
 import Button from './components/button';
 import Card from './components/card';
+import Icon from './components/icon';
 
 module.exports = {
   Button,
   Card,
   Column,
+  Icon,
   Row,
   Wrapper,
 };
