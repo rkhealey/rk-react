31b80541144ca8d29889cadc09b283beef70c577
rkhealey <hello@rkhealey.com>
2018-12-09 13:14:07 +1100

    fix dependancies

--- src/components/align/index.js
+++ src/components/align/index.js
@@ -5,13 +5,17 @@ const Align = styled.div`
   align-items: center;
   display: flex;
   justify-content: ${({ spacing }) => spacing};
+
+  ${({ overrides }) => overrides}
 `;
 
 Align.propTypes = {
+  overrides: PropTypes.shape({}),
   spacing: PropTypes.string,
 };
 
 Align.defaultProps = {
+  overrides: null,
   spacing: 'space-between',
 };
 
